22ddfab8c277644e1e2909fa24794c97c8c63c3a
Aditya Suresh <asuresh952@student.fuhsd.org>
2024-04-06 14:54:58 -0700

    sdsd
    sddsd

--- app.py
+++ app.py
@@ -6,12 +6,20 @@ app = Flask(__name__)
 
 @app.route("/")
 def mainpage():
-    return render_template("mainpage.html")
+    return render_template("mainpage.html", upload = url_for("upload"))
+
+@app.route("/upload")
+def upload():
+    return render_template("uploadphoto.html", results = url_for("results"))
+
+@app.route("/results")
+def results():
+    return render_template("results.html")
+
 
 def display_image():
     image = cv2.imread('images/cut5.jpeg', 0)
     cv2.imshow("text", image)
     cv2.waitKey(5)
     cv2.destroyAllWindows()
-    
-display_image()
+
