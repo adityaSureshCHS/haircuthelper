aa8c83e375b643ac645e3a8a401aada876c4bb4e
Aditya Suresh <asuresh952@student.fuhsd.org>
2024-04-06 13:45:02 -0700

    asd
    sdsd

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
-image = cv2.imread('cut5.jpeg')
-cv2.imwrite('cut5.jpeg', image)
-print("Image saved successfully.")
+image = cv2.imread('images/cut5.jpeg', 0)
+cv2.imshow('test', image);
+cv2.waitKey(0)
+cv2.destroyAllWindows()
