89d9295b823a67de242b57dbe6de487305ecbef8
Aditya Suresh <asuresh952@student.fuhsd.org>
2024-04-06 15:52:54 -0700

    jhj
    hhj

--- app.py
+++ app.py
@@ -12,14 +12,10 @@ def mainpage():
 def upload():
     return render_template("uploadphoto.html", results = url_for("results"))
 
-@app.route("/results")
+@app.route("/results", methods=["POST", "GET"])
 def results():
-    return render_template("results.html")
-
-
-def display_image():
-    image = cv2.imread('images/cut5.jpeg', 0)
-    cv2.imshow("text", image)
-    cv2.waitKey(5)
-    cv2.destroyAllWindows()
+    if request.method == 'POST':
+        return render_template("results.html")
+    else:
+        return render_template("results.html")
 
