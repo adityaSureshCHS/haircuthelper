9bf8334f4ab0fe0ad967e484bb71860541171527
Aditya Suresh <asuresh952@student.fuhsd.org>
2024-04-07 01:43:04 -0700

    oskdosd
    oksodks

--- app.py
+++ app.py
@@ -4,13 +4,16 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os
 import base64
+import openai
 from flask import Flask, url_for, request, render_template, redirect
 app = Flask(__name__)
 
 model = load_model("keras_model.h5", compile=False)
 class_names = open("labels.txt", "r").readlines()
 
-#hi
+openai.api_key = os.getenv("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")
+
+data, encoded_data, nparr, image, prediction, index, class_name, confidence_score
 
 @app.route("/")
 def mainpage():
@@ -45,9 +48,9 @@ def results():
         # Print prediction and confidence score
         print("Class:", class_name[2:], end="")
         print("Confidence Score:", str(np.round(confidence_score * 100))[:-2], "%")
-        
-        
-        return render_template("results.html")
-    else:
-        return render_template("results.html")
+    return redirect(url_for("display_styles"))
 
+
+@app.route("/display_results")
+def display_results():
+    return render_template("results.html")
